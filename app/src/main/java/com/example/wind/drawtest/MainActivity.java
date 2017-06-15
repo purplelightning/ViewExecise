d0aa141f606915b3f2f547b5f75fb32dbbcedcb6
wind <958031539@qq.com>
2017-06-15 20:29:23 +0800

    布局改成ScrollView,增加了四个动画文件 淡出,放大,平移,旋转.

--- app/src/main/java/com/example/wind/drawtest/MainActivity.java
+++ app/src/main/java/com/example/wind/drawtest/MainActivity.java
@@ -11,11 +11,12 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener,View.OnTouchListener
 {
     private WuziqiPanel wuziqiPanel;
-
+    private Button btn3,btn4,btn5,btn6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +24,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btn1=(Button)findViewById(R.id.paint);
         Button btn2=(Button)findViewById(R.id.wuziqi);
+        btn3=(Button)findViewById(R.id.trail1);
+        btn4=(Button)findViewById(R.id.trail2);
+        btn5=(Button)findViewById(R.id.trail3);
+        btn6=(Button)findViewById(R.id.trail4);
 
+        //点击监听
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
-
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        //触摸监听
         btn1.setOnTouchListener(this);
         btn2.setOnTouchListener(this);
     }
@@ -46,8 +56,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 v.clearAnimation();
                 break;
             case MotionEvent.ACTION_CANCEL:
-                v.clearAnimation();
                 break;
+
             default:
                 break;
         }
@@ -66,6 +76,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent2);
                 break;
 
+            case R.id.trail1:
+                Animation animation1= AnimationUtils.loadAnimation(MainActivity.this,R.anim.scale_to_big1);
+                v.startAnimation(animation1);
+                break;
+
+            case R.id.trail2:
+                Animation animation3=AnimationUtils.loadAnimation(MainActivity.this,R.anim.rotate_btn);
+                v.startAnimation(animation3);
+                break;
+
+            case R.id.trail3:
+                Animation animation4=AnimationUtils.loadAnimation(MainActivity.this,R.anim.alpha_btn);
+                v.startAnimation(animation4);
+                break;
+
+            case R.id.trail4:
+                Animation animation5=AnimationUtils.loadAnimation(MainActivity.this,R.anim.translate_btn);
+                v.startAnimation(animation5);
+                break;
             default:
                 break;
         }
